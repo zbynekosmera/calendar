68027eac7f53112db1293f33e2dccb106928d3f5
Zbynek <zbynek.osmera@N0509.hcg.homecredit.net>
2015-12-21 13:42:55 +0100

    Dalsi uprava, automaticky komit z prace.

---
+++ commit prace.bat
@@ -0,0 +1,13 @@
+echo start commitu
+
+git add -A
+
+git status
+
+git commit -m "Dalsi uprava, automaticky komit z prace."
+
+git push
+
+echo commit hotov
+
+pause
---
+++ update.bat
@@ -0,0 +1,9 @@
+echo start update
+
+git fetch
+
+git merge origin/master
+
+echo update hotovo
+
+pause
